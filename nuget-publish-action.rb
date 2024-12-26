fa30d83716530912ff3b0a04cae3de84d890e373
Test Alp <test@alpcoker.com>
2024-12-26 15:46:50 +0100

    .

--- nuget-publish-action.rb
+++ nuget-publish-action.rb
@@ -4,7 +4,7 @@ class NugetPublishAction < Formula
   desc "dotnet tool homebrew poc"
   homepage "https://github.com/test-alp/nuget-publish-action"
   url "https://www.nuget.org/api/v2/package/NugetPublishAction/0.0.16"
-  sha256 "b4b671036ad78e4fdffb7e39f4a259937b470c124db3274c66066dbc752431bd"
+  sha256 "sdfasdfas"
 
   depends_on DotnetRequirement
 
